262efa3a62670b76f07b16f7dff7c2072487d998
pasan lankathilaka <pasan.lankathilakadpl@gmail.com>
2026-03-08 15:29:17 +0000

    USE DIFFERNT ICON FOR SLOWLY PLAY BUTTON

--- src/components/AudioPlayback.tsx
+++ src/components/AudioPlayback.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Volume1, Volume2 } from 'lucide-react';
+import { Snail, Volume2 } from 'lucide-react';
 import { audioPlayer } from '@/lib/audioPlayer';
 import { Button } from '@/components/ui/button';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
@@ -31,7 +31,7 @@ export function AudioPlayback({ text, languageName }: AudioPlaybackProps) {
       <Tooltip>
         <TooltipTrigger asChild>
           <Button variant="ghost" size="icon" className="rounded-full" onClick={(e) => play(e, 0.7)}>
-            <Volume1 className="h-5 w-5" />
+            <Snail className="h-5 w-5" />
             <span className="sr-only">Play audio slowly for "{text}"</span>
           </Button>
         </TooltipTrigger>
