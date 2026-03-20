9a1f91b7afdbe18f493957b5df7dad4ab1b0a539
pasan lankathilaka <pasan.lankathilakadpl@gmail.com>
2026-03-20 18:48:47 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
   images: {
     remotePatterns: [
       {
