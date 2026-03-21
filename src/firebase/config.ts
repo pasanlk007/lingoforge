2ae2625b16692ae65ff3764fa592f7f10a3bdc33
pasan lankathilaka <pasan.lankathilakadpl@gmail.com>
2026-03-21 13:12:27 +0000

    still same error, guide e to fix

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  projectId: "studio-3754329818-ee8cf",
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
