a74338fed9ed85c4a5b687c8b6ad3c01565b0b1e
pasan lankathilaka <pasan.lankathilakadpl@gmail.com>
2026-03-22 05:56:18 +0000

    replace the good audio player, will support, web ,android ,apple

--- android/app/src/main/java/com/lingoforge/app/MainActivity.java
+++ android/app/src/main/java/com/lingoforge/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.lingoforge.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.tts.TextToSpeechPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    registerPlugin(TextToSpeechPlugin.class);
+  }
+}
