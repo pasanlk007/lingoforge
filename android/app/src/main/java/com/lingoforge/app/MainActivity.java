3b25091a9c478a186fc73fd1d1aec1b1145e31c1
pasan lankathilaka <pasan.lankathilakadpl@gmail.com>
2026-04-10 19:48:29 +0000

    feat: register GoogleAuth native plugin

--- android/app/src/main/java/com/lingoforge/app/MainActivity.java
+++ android/app/src/main/java/com/lingoforge/app/MainActivity.java
@@ -2,10 +2,12 @@ package com.lingoforge.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
+    registerPlugin(GoogleAuth.class);
     super.onCreate(savedInstanceState);
   }
 }
